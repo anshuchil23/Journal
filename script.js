6b737df7d4d62a75a88dd29ea7c90a02376a28c7
Isaac Sante <43215955+IsaacSante@users.noreply.github.com>
2020-09-23 21:37:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Wrap every letter in a span
+console.log('im working now');
 var textWrapper = document.querySelector('.container');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
@@ -7,7 +8,7 @@ anime.timeline({loop: true})
     targets: '.container .letter',
     opacity: [0,1],
     easing: "easeInOutQuad",
-    duration: 2250,
+    duration: 100,
     delay: (el, i) => 150 * (i+1)
   }).add({
     targets: '.container',
